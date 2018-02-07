b0b5d13786df0f1ebe01dd866fb2b8dba59396e4
Kerrvi Zen <30801244+kerrvi@users.noreply.github.com>
2018-02-07 15:26:50 +0800

    Add files via upload

--- js/controllers.js
+++ js/controllers.js
@@ -1,64 +1,81 @@
 ﻿angular.module('starter.controllers', [])
-
-.controller('LoginCtrl', function ($scope, $state, $ionicLoading, $ionicHistory, Login) {
-
-    $scope.data = {
-        loginCode: 'admin',
-        loginPwd: '1',
-    };
-
-    $scope.login = function () {
-        $state.go('desktop');
-        // Login.login($scope.data.loginCode, $scope.data.loginPwd)
-        // .then(function (data) {
-        //     $state.go('desktop');
-        // }, function (message) {
-        //     $ionicLoading.show({ template: '注册失败，' + message, noBackdrop: true, duration: 2000 });
-        // });
-    };
-
-    $scope.$on("$ionicView.beforeEnter", function () {
-        $ionicHistory.clearHistory();
-    });
-
-})
-
-.controller('HomeCtrl', function ($scope, $state, $ionicLoading, $ionicHistory,$ionicSideMenuDelegate, Utils, Login) {
-
-    $scope.goBack = function () {
-        if ($ionicHistory.backView()) {
-            $ionicHistory.goBack();
-        } else {
-            $state.go('home');
-        }
-    };
-
-    $scope.detail = function () {
-        $state.go('roomDetail');
-    }
-
-    $scope.toggleLeftSideMenu = function() {
-        $ionicSideMenuDelegate.toggleLeft();
-    };
-
-})
-
     .controller('indexCtrl', function ($scope, $state, $ionicLoading, $ionicHistory,$ionicSideMenuDelegate, Utils, Login) {
 
         $scope.goBack = function () {
             if ($ionicHistory.backView()) {
                 $ionicHistory.goBack();
             } else {
-                $state.go('home');
+                $state.go('index');
             }
         };
+
+        $scope.login = function () {
+            $state.go('login');
+        }
+
+        $scope.myOrder = function () {
+            $state.go('myOrder');
+        }
+
 
         $scope.detail = function () {
             $state.go('roomDetail');
         }
 
         $scope.toggleLeftSideMenu = function() {
-            $ionicSideMenuDelegate.toggleLeft();
+            $ionicSideMenuDelegate.toggleRight();
+        };
+
+    })
+
+    .controller('LoginCtrl', function ($scope, $state, $ionicLoading, $ionicHistory, Login) {
+
+        $scope.data = {
+            loginCode: 'admin',
+            loginPwd: '1',
+        };
+
+        $scope.goBack = function () {
+            if ($ionicHistory.backView()) {
+                $ionicHistory.goBack();
+            } else {
+                $state.go('index');
+            }
+        };
+
+        $scope.zhuce = function () {
+            $state.go('register');
+        }
+
+        $scope.login = function () {
+            $state.go('index');
+            // Login.login($scope.data.loginCode, $scope.data.loginPwd)
+            // .then(function (data) {
+            //     $state.go('desktop');
+            // }, function (message) {
+            //     $ionicLoading.show({ template: '注册失败，' + message, noBackdrop: true, duration: 2000 });
+            // });
+        };
+
+    $scope.$on("$ionicView.beforeEnter", function () {
+        // $ionicHistory.clearHistory();
+    });
+
+})
+
+    .controller('registerCtrl', function ($scope, $state, $ionicLoading, $ionicHistory, Login) {
+
+        $scope.data = {
+            loginCode: 'admin',
+            loginPwd: '1',
+        };
+
+        $scope.goBack = function () {
+            if ($ionicHistory.backView()) {
+                $ionicHistory.goBack();
+            } else {
+                $state.go('index');
+            }
         };
 
     })
@@ -69,7 +86,7 @@
             if ($ionicHistory.backView()) {
                 $ionicHistory.goBack();
             } else {
-                $state.go('home');
+                $state.go('index');
             }
         };
 
@@ -94,7 +111,23 @@
             if ($ionicHistory.backView()) {
                 $ionicHistory.goBack();
             } else {
-                $state.go('home');
+                $state.go('index');
+            }
+        };
+
+    })
+
+    .controller('myOrderCtrl', function ($scope, $state, $ionicLoading, $ionicHistory, Utils, Login) {
+        $scope.data = {
+            date:new Date(),
+            default:true
+        }
+
+        $scope.goBack = function () {
+            if ($ionicHistory.backView()) {
+                $ionicHistory.goBack();
+            } else {
+                $state.go('index');
             }
         };
 
--- js/routes.js
+++ js/routes.js
@@ -8,10 +8,20 @@ angular.module('starter.routes', [])
                 templateUrl: 'templates/login.html',
                 controller: 'LoginCtrl'
             })
-            .state('home',{
-                url: '/home',
-                templateUrl: 'templates/home.html',
-                controller: 'HomeCtrl'
+            .state('register', {
+                url: '/register',
+                templateUrl: 'templates/register.html',
+                controller: 'registerCtrl'
+            })
+            // .state('home',{
+            //     url: '/home',
+            //     templateUrl: 'templates/home.html',
+            //     controller: 'HomeCtrl'
+            // })
+            .state('index',{
+                url: '',
+                templateUrl: 'index.html',
+                controller: 'indexCtrl'
             })
             .state('roomDetail',{
                 url: '/roomDetail',
@@ -23,10 +33,15 @@ angular.module('starter.routes', [])
                 templateUrl: 'templates/room-list.html',
                 controller: 'roomListCtrl'
             })
+            .state('myOrder',{
+                url: '/myOrder',
+                templateUrl: 'templates/my-order.html',
+                controller: 'myOrderCtrl'
+            })
 
 
         ;
 
-         $urlRouterProvider.otherwise('/home');
+        // $urlRouterProvider.otherwise('/home');
 
     });
